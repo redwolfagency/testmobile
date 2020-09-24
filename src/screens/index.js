13a5870f81e9d5473cead22ce0acaf2c25cbf9f7
redwolfagency <benedicte.pancheri@gmail.com>
2020-09-24 15:57:39 +0200

    Fixing bug

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export { default as loginscreen } from './loginscreen/loginscreen'
+
+export { default as homescreen } from './homescreen/homescreen'
+
+export { default as registrationscreen } from './registrationscreen/registrationscreen'
